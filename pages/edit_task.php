9b0f3a341d287560d8a2f19590f6cf642ccb73e5
sainath g dutkar <sgd23@afsaccess1.njit.edu>
2017-12-17 12:39:52 -0500

    validation for todo task

--- pages/edit_task.php
+++ pages/edit_task.php
@@ -18,20 +18,52 @@
 <body>
 
 
-<form action="index.php?page=tasks&action=addtask&id=<?php echo $data->id; ?>" method="POST">
+<form name="updateTask" action="index.php?page=tasks&action=addtask&id=<?php echo $data->id; ?>" onsubmit="return validateForm()" method="POST">
     Owner email: <input type="text" name="owneremail" value="<?php echo $data->owneremail;?>"><br>
     Owner id: <input type="text" name="ownerid" value="<?php echo $data->ownerid;?>" readonly><br>
-    Created date: <input type="text" name="cdate" value="<?php echo $data->createddate;?>"><br>
-    due date: <input type="text" name="ddate" value="<?php echo $data->duedate;?>"><br>
-    message: <input type="text" name="message" value="<?php echo $data->message;?>"><br>
+    Created date: <input type="text" name="cdate" value="<?php echo $data->createddate;?>" readonly><br>
+    due date: <input type="text" name="ddate" value="<?php echo $data->duedate;?>" readonly><br>
+    message: <input type="text" name="message" value="<?php echo $data->message;?>" required><br>
     isdone: <input type="text" name="isdone" value="<?php echo $data->isdone;?>"><br>
-    <button type="submit">UPDATE</button>
+    <input type="submit" value="UPDATE">
 </form>
 
-
-
-
 <script src="js/scripts.js"></script>
+<script>
+    function validateForm() {
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        var email = document.forms["updateTask"]["owneremail"].value;
+        var isdone = document.forms["updateTask"]["isdone"].value;
+        var temp;
+       // alert(isdone);
+        var alertmessage = "";
+
+
+        if (email == "")
+        {
+            alertmessage = alertmessage + "Please enter the Email address  ,";
+        }
+        if (!email.match(mailformat) && email != "")
+        {
+            alertmessage = alertmessage + "Email address is not Valid ,";
+        }
+        if (isdone == "1" || isdone == "0"){temp = true}
+
+        if(!temp){
+            alertmessage = alertmessage + "Please enter BINARY value 0/1 for IS DONE.";
+        }
+
+        if (alertmessage != "")
+        {
+            alert(alertmessage);
+            return false;
+        }
+
+    }
+</script>
+
+
+
 </body>
 </html>
 
